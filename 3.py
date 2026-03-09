9a21f17abc702b4236259490e801e16dcd5d7f94
Oitech.mcp <oitechopensourcemcp5471069inc@gmail.com>
2026-03-09 19:16:58 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,18 @@
-# 3. DFS
+# DFS (Depth First Search)
+
 def dfs(graph, node, visited=None):
-    if visited is None: visited = set()
-    visited.add(node); print(node, end=' ')
+    if visited is None:
+        visited = set()
+
+    visited.add(node)
+    print(node, end=' ')
+
     for n in graph[node]:
-        if n not in visited: dfs(graph, n, visited)
+        if n not in visited:
+            dfs(graph, n, visited)
+
+# Graph definition
+graph = {0:[1,2], 1:[0,3,4], 2:[0], 3:[1], 4:[1]}
 
 dfs(graph, 0)
 print()
