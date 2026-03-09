e3eff4a880ba3cc3a4b1fb2029f0c98f3a6ce38f
Oitech.mcp <oitechopensourcemcp5471069inc@gmail.com>
2026-03-09 19:16:09 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,14 +1,23 @@
-# 2. BFS
+# BFS (Breadth First Search)
+
+from collections import deque   # Import deque
+
 def bfs(graph, start):
-    visited, queue = set(), deque([start])
+    visited = set()
+    queue = deque([start])
+
     visited.add(start)
+
     while queue:
         node = queue.popleft()
         print(node, end=' ')
+
         for n in graph[node]:
             if n not in visited:
-                visited.add(n); queue.append(n)
+                visited.add(n)
+                queue.append(n)
 
-graph = {0:[1,2],1:[0,3,4],2:[0],3:[1],4:[1]}
+graph = {0:[1,2], 1:[0,3,4], 2:[0], 3:[1], 4:[1]}
+
 bfs(graph, 0)
 print()
